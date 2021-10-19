5aae6467626b76f7877c9e6bce420c2a36154a8d
Welington Borba Cordeiro <wellinkeed@gmail.com>
2021-10-19 00:42:21 -0300

    commit inicial

---
+++ src/routes.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { Get3LastMessagesController } from "./controllers/GetLast3MessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
+
+const router = Router();
+
+router.post("/authenticate", new AuthenticateUserController().handle)
+
+router.post("/messages",
+    ensureAuthenticated,
+    new CreateMessageController().handle
+
+);
+
+router.get("/messages/last3", new Get3LastMessagesController().handle)
+
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle);
+
+export { router }
---
+++ src/server.ts
@@ -0,0 +1,6 @@
+import { serverHttp } from "./app";
+
+
+serverHttp.listen(4000, () =>
+    console.log(` :rocket: Server is running on PORT 4000`)
+);
